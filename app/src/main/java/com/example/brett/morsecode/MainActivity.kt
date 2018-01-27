6c2cee0dfc95a0e9a24594104b0f2b5b06fa0fff
Brett <Brett@BRETT-PC>
2018-01-27 13:08:33 -0600

    added new appendTextAndScroll/hidekeyboard functions.

--- app/src/main/java/com/example/brett/morsecode/MainActivity.kt
+++ app/src/main/java/com/example/brett/morsecode/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.brett.morsecode
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.method.ScrollingMovementMethod
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -16,15 +20,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        hideKeyboard()
         //..needed for scrolling
-        textView.movementMethod = ScrollingMovementMethod();
+        mTextView.movementMethod = ScrollingMovementMethod();
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
 
-        
+        testButton.setOnClickListener{ view->
+            appendTextAndScroll(inputText.text.toString())
+            hideKeyboard()
+        }
+    }
+
+    private fun appendTextAndScroll(text: String){
+        if (mTextView != null){
+            mTextView.append(text + "\n")
+            val layout = mTextView.getLayout()
+            if(layout != null){
+                val scrollDelta = (layout.getLineBottom(mTextView.getLineCount() - 1) - mTextView.getScrollY()
+                        - mTextView.getHeight())
+                if(scrollDelta > 0)
+                    mTextView.scrollBy(0, scrollDelta)
+            }
+        }
+    }
+
+    fun Activity.hideKeyboard(){
+        hideKeyboard(if (currentFocus == null) View(this) else currentFocus)
+    }
+
+    fun Context.hideKeyboard(view: View){
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
