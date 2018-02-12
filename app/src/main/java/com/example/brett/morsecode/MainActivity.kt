99bb8a0f93cd870415200f209362e4dc231c8dd4
Brett <Brett@BRETT-PC>
2018-02-12 17:03:58 -0600

    beep code written. not working

--- app/src/main/java/com/example/brett/morsecode/MainActivity.kt
+++ app/src/main/java/com/example/brett/morsecode/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.brett.morsecode
 
 import android.app.Activity
 import android.content.Context
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -11,10 +14,12 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import org.json.JSONObject
+import java.lang.Math.round
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,6 +62,16 @@ class MainActivity : AppCompatActivity() {
 
         clearButton.setOnClickListener{ view ->
             mTextView.setText("")
+        }
+
+        play_button.setOnClickListener{ view ->
+            var textInBox = translateTextToMorse(inputText.text.toString())
+
+            var i:Int = 0
+            while(i != 50){
+                playDot()
+                i++
+            }
         }
     }
 
@@ -174,6 +189,100 @@ class MainActivity : AppCompatActivity() {
         for(k in letToCodeDict.keys.sorted())
             appendTextAndScroll("$k: ${letToCodeDict[k]}")
     }
+
+    fun playString(s:String, i: Int = 0) : Unit{
+        if (i > s.length - 1)
+            return;
+
+        var mDelay: Long = 0
+        // thenFun = lambda function that will
+        // switch back to main thread and play the next char
+        var thenFun: () -> Unit = { ->
+            this@MainActivity.runOnUiThread(java.lang.Runnable{
+                playString(s, i+1)
+            })
+        }
+
+        var c = s[i]
+        Log.d("log", "Processing pos: " + i + " char: {" + c + "]")
+        if(c == '.')
+            playDot(thenFun)
+        else if(c == '-')
+            playDash(thenFun)
+        else if(c == '/')
+            pause(6*dotLength, thenFun)
+        else if(c == ' ')
+            pause(2*dotLength, thenFun)
+    }
+
+    val dotLength:Int = 50
+    val dashLength:Int = dotLength * 3
+
+    val dotSoundBuffer:ShortArray = genSineWaveSoundBuffer(550.0, dotLength)
+    val dashSoundBuffer:ShortArray = genSineWaveSoundBuffer(550.0, dashLength)
+
+    fun playDash(onDone : () -> Unit = { /* noop */}){
+        Log.d("DEBUG", "playDash")
+        playSoundBuffer(dashSoundBuffer, { -> pause(dotLength,onDone)})
+    }
+
+    fun playDot(onDone : () -> Unit = { /* noop */ }){
+        Log.d("DEBUG", "playDot")
+        playSoundBuffer(dotSoundBuffer, { -> pause(dotLength, onDone)})
+    }
+
+    fun pause(durationMSec: Int, onDone : () -> Unit = { /* noop */ }){
+        Log.d("DEBUG", "pause: " + durationMSec)
+        Timer().schedule(timerTask{onDone()}, durationMSec.toLong())
+    }
+
+    val SAMPLE_RATE = 44100
+
+    private fun genSineWaveSoundBuffer(frequency: Double, durationMSec: Int) : ShortArray{
+        val duration:Int = round((durationMSec / 1000.0) * SAMPLE_RATE).toInt()
+
+        var mSound : Double
+        val mBuffer = ShortArray(duration)
+        for(i in 0 until duration ) {
+            mSound = Math.sin(2.0 * Math.PI * i.toDouble() / (SAMPLE_RATE / frequency))
+            mBuffer[i] = (mSound * java.lang.Short.MAX_VALUE).toShort()
+        }
+        return mBuffer
+    }
+
+    private fun playSoundBuffer(mBuffer:ShortArray, onDone : () -> Unit = { /* noop */}){
+        var minBufferSize = SAMPLE_RATE/10
+        if(minBufferSize < mBuffer.size){
+            minBufferSize = minBufferSize + minBufferSize *
+                    (Math.round(mBuffer.size.toFloat()) / minBufferSize.toFloat() ).toInt()
+        }
+
+        val nBuffer = ShortArray(minBufferSize)
+        for(i in nBuffer.indices){
+            if (i < mBuffer.size) nBuffer[i] = mBuffer[i]
+            else nBuffer[i] = 0
+        }
+
+        val mAudioTrack = AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE,
+                AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
+                minBufferSize, AudioTrack.MODE_STREAM)
+
+        mAudioTrack.setStereoVolume(AudioTrack.getMaxVolume(), AudioTrack.getMaxVolume())
+        mAudioTrack.setNotificationMarkerPosition(mBuffer.size)
+        mAudioTrack.setPlaybackPositionUpdateListener(object: AudioTrack.OnPlaybackPositionUpdateListener{
+            override fun onPeriodicNotification(track: AudioTrack) {
+            }
+            override fun onMarkerReached(track: AudioTrack) {
+                Log.d("Log", "Audio track end of file reach...")
+                mAudioTrack.stop()
+                mAudioTrack.release()
+                onDone()
+            }
+        })
+        mAudioTrack.play()
+        mAudioTrack.write(nBuffer, 0, minBufferSize)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
